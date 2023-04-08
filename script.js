59c48b57cad3df6b10919ad7046e2b16b371ac3a
AndriiKot <a_kocyuba@ukr.net>
2023-04-08 18:44:50 +0200

    Creation new relis version 2.0

--- script.js
+++ script.js
@@ -6,25 +6,28 @@ const i_max = photos.length - 1;
 const i_min = 0;
 let i_next = i_min;
 
+const empty = '';
+const showed = 'showed';
+
 
 
 button_next.onclick = function() {
-  photos[i_next].style.display = 'none';
+  photos[i_next].className = empty;
   if (i_next < i_max) {
     i_next++;
   } else {
     i_next = i_min;
   }
-  photos[i_next].style.display = 'block'
+  photos[i_next].className = showed;
 }
 
 button_prev.onclick = function() {
-  photos[i_next].style.display = 'none';
+  photos[i_next].className = empty;
   if (i_next > i_min) {
     i_next--;
   } else {
     i_next = i_max;
   }
-  photos[i_next].style.display = 'block';
+  photos[i_next].className = showed;
 }
 
