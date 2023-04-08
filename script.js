38e496b739c5eea61656a81a57e836bf225dc9f6
AndriiKot <a_kocyuba@ukr.net>
2023-04-08 16:22:44 +0200

    Add buttons elements and arr photos

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-﻿let button = document.querySelector('button')
-button.onclick = function() {
-  alert("Hello, user!");
-  console.dir(button)
+﻿const button_prev = document.querySelector('.prev');
+const button_next = document.querySelector('.next');
+const photos = document.querySelectorAll('.photos');
+let i = 0;
+
+button_prev.onclick = function() {
+  photos[i].hidden = true;
+  console.dir(photos[i])
+  // console.log(photos)
 }
+
