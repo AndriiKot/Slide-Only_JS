96420d98a2857896046eadfcd0e52ed807af0855
AndriiKot <a_kocyuba@ukr.net>
2023-04-08 17:54:20 +0200

    fixed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 ﻿const button_prev = document.querySelector('.prev');
 const button_next = document.querySelector('.next');
 const photos = document.querySelectorAll('.photos img');
-const step = 1;
 const i_max = photos.length - 1;
 const i_min = 0;
 let i_next = i_min;
+
 
 
 button_next.onclick = function() {
@@ -12,14 +12,14 @@ button_next.onclick = function() {
   if (i_next < i_max) {
     i_next++;
   } else {
-    i_next = i_min
+    i_next = i_min;
   }
   photos[i_next].style.display = 'block'
 }
 
 button_prev.onclick = function() {
   photos[i_next].style.display = 'none';
-  if (i_next > 0) {
+  if (i_next > i_min) {
     i_next--;
   } else {
     i_next = i_max;
