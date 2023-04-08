157ce3a67f0808f14febc12b5052c7f54df1c52e
AndriiKot <a_kocyuba@ukr.net>
2023-04-08 17:47:09 +0200

    Add button prev

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 ﻿const button_prev = document.querySelector('.prev');
 const button_next = document.querySelector('.next');
 const photos = document.querySelectorAll('.photos img');
-let i_next = 0;
-let i_prev = photos.lenght - 1;
+const step = 1;
+const i_max = photos.length - 1;
+const i_min = 0;
+let i_next = i_min;
+
 
 button_next.onclick = function() {
   photos[i_next].style.display = 'none';
-  i_next++;
+  if (i_next < i_max) {
+    i_next++;
+  } else {
+    i_next = i_min
+  }
   photos[i_next].style.display = 'block'
+}
+
+button_prev.onclick = function() {
+  photos[i_next].style.display = 'none';
+  if (i_next > 0) {
+    i_next--;
+  } else {
+    i_next = i_max;
+  }
+  photos[i_next].style.display = 'block';
 }
 
